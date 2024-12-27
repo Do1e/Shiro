4b5f33fd24d8550e3572f614c951e933cc583cb2
Do1e <dpj.email@qq.com>
2024-12-28 00:21:10 +0800

    chore: add page support in reading list

--- src/providers/root/socket-provider.tsx
+++ src/providers/root/socket-provider.tsx
@@ -8,6 +8,7 @@ import { useSocketIsConnect, useSocketSessionId } from '~/atoms/hooks'
 import { usePageIsActive } from '~/hooks/common/use-is-active'
 import { useIsClient } from '~/hooks/common/use-is-client'
 import { useCurrentNoteDataSelector } from '~/providers/note/CurrentNoteDataProvider'
+import { useCurrentPageDataSelector } from '~/providers/page/CurrentPageDataProvider'
 import { useCurrentPostDataSelector } from '~/providers/post/CurrentPostDataProvider'
 import { SocketEmitEnum } from '~/types/events'
 
@@ -16,7 +17,8 @@ import { socketWorker } from '../../socket/worker-client'
 export const useCurrentArticleId = () => {
   const noteId = useCurrentNoteDataSelector((data) => data?.data?.id)
   const postId = useCurrentPostDataSelector((data) => data?.id)
-  return noteId || postId
+  const pageId = useCurrentPageDataSelector((data) => data?.id)
+  return noteId || postId || pageId
 }
 
 if (typeof window !== 'undefined') {
@@ -43,9 +45,6 @@ const SocketContainerImpl: Component = () => {
   const socketIsConnected = useSocketIsConnect()
   const currentArticleId = useCurrentArticleId()
   const previousArticleIdRef = useRef<string | null>(null)
-
-  console.log('socketIsConnected', socketIsConnected)
-  console.log('webSocketSessionId', webSocketSessionId)
   console.log('currentArticleId', currentArticleId)
 
   useEffect(() => {
